964032e329d8e886085b0863f549c6305cc0418a
aitsym <saidaitsym@gmail.com>
2022-03-28 00:23:18 +0000

    Heroku config files and updates

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#kts_e#8m=bt#jld!5rm44xselp$16wla%gbdhq(i*+ityfhvv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
